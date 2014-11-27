22cc38697d7f15e79f088a38ca271b5395b7b9f6
Amit Burstein <amit.burstein@gmail.com>
2014-11-26 19:42:47 -0800

    Initialize project

---
+++ Palettes/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Palettes
+//
+//  Created by Amit Burstein on 11/26/14.
+//  Copyright (c) 2014 Amit Burstein. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+}
---
+++ Palettes/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Palettes
+//
+//  Created by Amit Burstein on 11/26/14.
+//  Copyright (c) 2014 Amit Burstein. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+}
