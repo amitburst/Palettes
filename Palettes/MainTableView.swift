e3f08eadb5d7affe0ece47cdc7ded37d48e8a89b
Amit Burstein <amit.burstein@gmail.com>
2014-12-02 13:45:38 -0800

    Show more results when scrolled to bottom of table view, closes #10

--- Palettes/MainTableView.swift
+++ Palettes/MainTableView.swift
@@ -19,22 +19,4 @@ class MainTableView: NSTableView {
         super.drawGridInClipRect(finalClipRect)
     }
     
-    // MARK: Functions
-    
-    func scrollRowToVisible(row: Int, animate: Bool) {
-        if (animate) {
-            let rowRect = rectOfRow(row)
-            var scrollOrigin = rowRect.origin
-            let clipView = superview as NSClipView
-            scrollOrigin.y += max(0, round((rowRect.height - clipView.frame.height) * 0.5));
-            let scrollView = clipView.superview as NSScrollView
-            if scrollView.respondsToSelector("flashScrollers") {
-                scrollView.flashScrollers()
-            }
-            clipView.animator().setBoundsOrigin(scrollOrigin)
-        } else {
-            scrollRowToVisible(row)
-        }
-    }
-    
 }
